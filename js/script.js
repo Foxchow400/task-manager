491b1ff073605052069f2c7630ace3fecebb0774
Foxchow400 <foxchow400@outlook.com>
2022-07-28 14:15:31 -0600

    added everything but description updates

--- js/script.js
+++ js/script.js
@@ -33,27 +33,47 @@ function createTask() {
 
 function updateTaskList(taskList) {
     //update the button ids to make them match the array
-    //display each item in the array    
-    var arrayResult = [];
+    //display each item in the array   
     var taskListStr = "";
     var taskListCopy = taskList;
     console.log(taskListCopy.length);
+    //this if statement uses the i variable as an index to make new tasks with unique id's, this also updates the title and description data.
     if (taskListCopy.length == 1) {
-        arrayResult = taskListCopy[0];
-        taskListStr += "<div class='card'><input type='text' class='t' onkeydown='updateValue()' value='" +  arrayResult[0] + "'><button id='0' onClick='deleteItem(0)'>x</button><input type='text' class='d' value='" +  arrayResult[0] + "'></div>";
+        //this if statement first executes the exception for the event i = 0 (the index is zero, if we tell it to be 0 - 1 it will not work because it is a negative) 
+        currentTask = taskListCopy[0];
+        taskListStr += "<div class='card'><input type='text' id='0' class='t' onkeydown='updateTitleValue(event)' value='" +  currentTask[0] + "'><button id='0' onClick='deleteItem(0)'>x</button><input type='text' class='d' onkeydown='updateDescValue(event)' value='" +  currentTask[1] + "'></div>";
     } else {
+        //this for loop defines what happens the rest of the time, when i > 0 (if i is greater than zero then subtracting one is fine)
         for (i = 0; i < taskListCopy.length; i++) {
-            arrayResult = taskListCopy[i];
-            taskListStr += "<div class='card'><input type='text' class='t' onkeydown='updateValue()' value='" +  arrayResult[0] + "'><button id='" + i + "' onClick='deleteItem(" + i + ")'>x</button><input type='text' class='d' onkeydown='updateValue()' value='" +  arrayResult[0] + "'></div>";
+            currentTask = taskListCopy[i];
+            console.log(currentTask);
+            taskListStr += "<div class='card'><input type='text' id='" + i +
+            "' class='t' onkeydown='updateTitleValue(event)' value='" +  currentTask[0] +
+            "'><button id='" + i +
+            "' onClick='deleteItem(" + i +
+            ")'>x</button><input type='text' class='d " + i + "' desc='test' onkeydown='updateDescValue(event)' value='" +
+            currentTask[1] + "'></div>";
         };
     };
     taskListElement.innerHTML = taskListStr;
 }
 
-//function that sets the value of the current array input based on the button id
-function updateValue() {
+
+//function that sets the value of the current title array input based on the button id
+function updateTitleValue(event) {
     if (event.key === "Enter") {
-        console.log("test");
+        taskList[event.target.id][0] = event.target.value;
+        updateTaskList(taskList);
+    }
+}
+//function that sets the value of the current description array input based on the button id
+function updateDescValue(event) {
+    if (event.key === "Enter") {
+        console.log(event);
+        console.log(document.getElementByDesc)
+        console.log(taskList[event.target.desc]);
+        taskList[document.getElementsByClassName(i)][1] = event.target.value;
+        updateTaskList(taskList);
     }
 }
 
